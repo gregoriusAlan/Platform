95a685bcd49761744702efe9c7b2d0a710f4ae14
gregoriusAlan <alanwijaya404@mail.com>
2024-03-13 10:33:08 +0700

    menambahkan div form

--- Tugas4/scripTugas4.js
+++ Tugas4/scripTugas4.js
@@ -18,3 +18,30 @@ function tombolOk() {
         inputPilih();
     }
 }
+
+function inputPilih() {
+    btn.remove();
+    const element = document.querySelectorAll('.form-group')
+    element.forEach(function(e) {
+        e.parentNode.removeChild(e);
+    });
+
+    var batas = parseInt(inputPilihan.value.trim());
+    for (let index = 0; index < batas; index++) {
+        const pilih = document.createElement('div');
+        pilih.classList.add('form-group');
+
+        const label = document.createElement('label');
+        label.textContent = 'pilihan' + (index + 1) + ':';
+        label.setAttribute('for', 'pilihan');
+
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.setAttribute('id', 'pilihan');
+
+
+        pilih.appendChild(label);
+        pilih.appendChild(input);
+        container.appendChild(pilih);
+    }
+   
