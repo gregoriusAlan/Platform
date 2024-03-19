96042dc23b0c0def6eb1198e459d88a6db9d5c3a
gregoriusAlan <alanwijaya404@mail.com>
2024-03-19 13:19:54 +0700

    membetulkan html

--- Tugas4/scriptTugas4.js
+++ Tugas4/scriptTugas4.js
@@ -1,14 +1,15 @@
-var namaInput = document.querySelector('#namaInput');
-var pilihanInput = document.querySelector('#pilihanInput');
-var container = document.querySelector('.container')
-const btnOk = document.querySelector('#btnOk');
+
+var namaInput = document.querySelector('#name');
+var pilihanInput = document.querySelector('#pilihan');
+var container = document.querySelector('.contain');
+var btnOk = document.querySelector('#btn');
 
 var tampung = [];
 var pilihan;
 var nama;
+var txtARy = '';
 
-btnOk.addEventListener('click', tombolOk);
-
+btnOk.addEventListener('click',tombolOk);
 function tombolOk() {
     nama = namaInput.value.trim();
     pilihan = pilihanInput.value;
@@ -26,10 +27,10 @@ function inputPilih() {
         e.parentNode.removeChild(e);
     });
 
-    var batas = parseInt(inputPilihan.value.trim());
+    var batas = parseInt(pilihanInput.value.trim());
     for (let index = 0; index < batas; index++) {
         const pilih = document.createElement('div');
-        pilih.classList.add('form-group');
+        pilih.classList.add('Tugas4.html');
 
         const label = document.createElement('label');
         label.textContent = 'pilihan' + (index + 1) + ':';
@@ -44,4 +45,22 @@ function inputPilih() {
         pilih.appendChild(input);
         container.appendChild(pilih);
     }
+
+    const ok = document.createElement('button');
+    ok.type = 'submit';
+    ok.setAttribute('id', 'btnOK');
+    ok.textContent = 'OK';
+    container.appendChild(ok);
+
+    const okb = document.querySelector('#btnOK');
+    okb.addEventListener('click', function(e) {
+        const inp = document.querySelectorAll('#pilihan')
+        for (let index = 0; index < inp.length; index++) {
+            tamp[index] = inp[index].value;
+
+        }
+        inputHobi();
+        ok.disabled = true;
+    });
+}
    
